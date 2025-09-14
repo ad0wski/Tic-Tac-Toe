816e94d3033eced943014c66bccf265cc814e685
Adam <ak.jurkowski@gmail.com>
2025-09-14 16:59:32 +0200

    Poczatek programu i dodanie wypiywania planszy

--- Tic-Tac-Toe-Game.py
+++ Tic-Tac-Toe-Game.py
@@ -1,1 +1,19 @@
-Game here!
+plansza = [[""], [""], [""],
+           [""], [""], [""],
+           [""], [""], [""]]
+
+def wypisaniePlanszy(plansza):
+    print("\n")
+    for i in range(3):
+        for j in range(3):
+            if j < 2:
+                print(f" a ", end="|")
+            else:
+                print(f" a ")
+        if i < 2:
+            print("─── ─── ───")
+    print("\n")
+
+
+
+wypisaniePlanszy(plansza)
